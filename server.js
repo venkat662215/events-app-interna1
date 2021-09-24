e79ca72c83448e2d86ef020c7bef76a052cbe605
venkat662215 <mvenkata.scm@gmail.com>
2021-09-24 05:40:39 +0000

    updated

--- server.js
+++ server.js
@@ -32,8 +32,8 @@ const mockEvents = {
 };
 
 // health endpoint - returns an empty array
-app.get('/', (req, res) => {
-    res.json([]);
+app.get('/events', (req, res) => {
+    getEvents(req, res);
 });
 
 // version endpoint to provide easy convient method to demonstrating tests pass/fail
@@ -67,7 +67,47 @@ app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).json({ message: err.message });
 });
+// bring in firestore
+const Firestore = require("@google-cloud/firestore");
 
+// initialize Firestore and set project id from env var
+const firestore = new Firestore(
+    {
+        projectId: process.env.GOOGLE_CLOUD_PROJECT
+    }
+);
+app.post('/event', (req, res) => {
+    // create a new object from the json data and add an id
+    const ev = { 
+        title: req.body.title, 
+        description: req.body.description,
+        id : mockEvents.events.length + 1
+     }
+// this will create the Events collection if it does not exist
+    firestore.collection("Events").add(ev).then(ret => {
+        getEvents(req, res);
+    });
+
+});
+function getEvents(req, res) {
+    firestore.collection("Events").get()
+        .then((snapshot) => {
+            if (!snapshot.empty) {
+                const ret = { events: []};
+                snapshot.docs.forEach(element => {
+                    ret.events.push(element.data());
+                }, this);
+                console.log(ret);
+                res.json(ret);
+            } else {
+                 res.json(mockEvents);
+            }
+        })
+        .catch((err) => {
+            console.error('Error getting events', err);
+            res.json(mockEvents);
+        });
+};
 const PORT = process.env.PORT ? process.env.PORT : 8082;
 const server = app.listen(PORT, () => {
     const host = server.address().address;
